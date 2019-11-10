255becd67afa2b0f697dafdf507d5f2322f6b563
J.David Luque <jdavidls@gmail.com>
2019-11-10 22:13:34 +0100

    remove unused modules

--- projects/prodisign/rh/client/src/layout/index.ts
+++ projects/prodisign/rh/client/src/layout/index.ts
@@ -1,9 +1,1 @@
-export * from './LayoutService';
-export * from './LayoutViewportService';
-export * from './LayoutSearchService';
-
-export * from './LayoutNavbar';
-export * from './LayoutSidenav';
-export * from './LayoutModalViewport';
-export * from './LayoutContent';
 export * from './LayoutModule';
